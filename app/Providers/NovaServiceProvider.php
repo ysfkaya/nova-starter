4fa13aceec57443758ff021742fbae999bfff47c
ysfkayaa <yusuf.kaya.x0@gmail.com>
2021-03-05 00:17:40 +0300

    added notification tool

--- app/Providers/NovaServiceProvider.php
+++ app/Providers/NovaServiceProvider.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\Gate;
 use Laravel\Nova\Cards\Help;
 use Laravel\Nova\Nova;
 use Laravel\Nova\NovaApplicationServiceProvider;
+use Mirovit\NovaNotifications\NovaNotifications;
 use Ysfkaya\Settings\SettingTool;
 
 class NovaServiceProvider extends NovaApplicationServiceProvider
@@ -78,7 +79,8 @@ class NovaServiceProvider extends NovaApplicationServiceProvider
     public function tools()
     {
         return [
-            SettingTool::make()
+            SettingTool::make(),
+            NovaNotifications::make()
         ];
     }
 
